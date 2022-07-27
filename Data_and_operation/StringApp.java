745d44d2edb68226698fb98a59691f8f1f6959b0
lKori <yuy0841@gmail.com>
2022-07-28 01:19:03 +0900

    220727_java

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,18 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World"); // ""는 String(Character의 모임)
+		System.out.println('H'); // ''는 Character(한 글자만 표현)
+		System.out.println("H");
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		//new line
+		System.out.println("Hello \nWorld");
+		System.out.println("Hello \"World\""); // Hello "World"
+	}
+
+}
