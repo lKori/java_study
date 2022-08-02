312875297230a6b4f64080bde711a0800b24c539
lKori <yuy0841@gmail.com>
2022-08-02 22:29:07 +0900

    20220802_java
    생활코딩 JAVA1 완

--- MyApp/AccountingArrayApp.java
+++ MyApp/AccountingArrayApp.java
@@ -1,5 +1,5 @@
 
-public class AccountingApp {
+public class AccountingArrayApp {
 
 	public static void main(String[] args) {
 		double valueOfSupply = Double.parseDouble(args[0]);
@@ -9,9 +9,15 @@ public class AccountingApp {
 		double total = valueOfSupply + vat;
 		double expense = valueOfSupply * expenseRate;
 		double income = valueOfSupply - expense;
-		double dividend1 = income * 0.5;
-		double dividend2 = income * 0.3;
-		double dividend3 = income * 0.2;
+		
+		double[] dividendRates = new double[3];
+		dividendRates[0] = 0.5;
+		dividendRates[1] = 0.3;
+		dividendRates[2] = 0.2;
+
+		double dividend1 = income * dividendRates[0];
+		double dividend2 = income * dividendRates[1];
+		double dividend3 = income * dividendRates[2];
 		
 		System.out.println("Value of supply : " + valueOfSupply);
 		System.out.println("VAT : " + vat);
--- MyApp/AccountingArrayLoopApp.java
+++ MyApp/AccountingArrayLoopApp.java
@@ -1,5 +1,5 @@
 
-public class AccountingApp {
+public class AccountingArrayLoopApp {
 
 	public static void main(String[] args) {
 		double valueOfSupply = Double.parseDouble(args[0]);
@@ -9,19 +9,23 @@ public class AccountingApp {
 		double total = valueOfSupply + vat;
 		double expense = valueOfSupply * expenseRate;
 		double income = valueOfSupply - expense;
-		double dividend1 = income * 0.5;
-		double dividend2 = income * 0.3;
-		double dividend3 = income * 0.2;
-		
+
 		System.out.println("Value of supply : " + valueOfSupply);
 		System.out.println("VAT : " + vat);
 		System.out.println("Total : " + total);
 		System.out.println("Expense : " + expense);
 		System.out.println("Income : " + income);
-		System.out.println("Dividend 1 : " + dividend1);
-		System.out.println("Dividend 2 : " + dividend2);
-		System.out.println("Dividend 3 : " + dividend3);
 		
+		double[] dividendRates = new double[3];
+		dividendRates[0] = 0.5;
+		dividendRates[1] = 0.3;
+		dividendRates[2] = 0.2;
+		
+		int i = 0;
+		while(i < dividendRates.length) {
+			System.out.println("Dividend : " + (income * dividendRates[i]));
+			 i += 1;
+		}
 	}
 
 }
